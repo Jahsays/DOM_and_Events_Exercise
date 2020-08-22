2cd9471b1785acff6aef0a18699fccf832f1f9ae
Chiaka Williams <Chiaka394@gmail.com>
2020-08-22 23:12:17 +0100

    Added Events file

---
+++ Events/script.js
@@ -0,0 +1,24 @@
+// Part 1
+// Add the necessary code to wait for the DOM to load to make sure that anything you manipulate in the DOM has loaded. You can do this either using window.onload or adding an event listener for DOMContentLoaded.
+document.addEventListener("DOMContentLoaded", function(){
+    })
+
+// Replace the text "Change me" with "Hello World!".
+var change_heading = document.getElementById("change_heading");
+    change_heading.innerHTML = "Hello World!"
+
+// When a user hovers over one of the colored boxes change the text to display the color that is being hovered over.
+var section = document.querySelector("section");
+section.addEventListener("mouseover", function(event){
+   var selectedColor = document.querySelector('.selected');
+   selectedColor.innerText = event.target.className;
+ })
+
+// Create a new div element.
+var newDiv = document.createElement("div");
+
+// Give your new div a class of purple and style it so that it has a background color of purple.
+newDiv.className = "Purple"
+
+// Append your new div to the page to the section tag.
+section.apppendChild(newDiv)
